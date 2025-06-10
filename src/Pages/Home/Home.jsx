5f4c2c5fa5f8d232e37d7e8904660e146c23132d
tonmoy <tonmoyth13@gmail.com>
2025-06-10 12:52:22 +0600

    added service section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,16 +2,17 @@ import Banner from './Banner/Banner';
 import Map from './Map';
 import FeaturedRooms from './FeaturedRooms/FeaturedRooms';
 import OurHotel from './OurHotel/OurHotel';
-// import Service from './Service/Service';
+import Service from './Service/Service';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             {/* <Map></Map> */}
-            <FeaturedRooms></FeaturedRooms>
             <OurHotel></OurHotel>
-            {/* <Service></Service> */}
+            <FeaturedRooms></FeaturedRooms>
+            
+            <Service></Service>
         </div>
     );
 };
--- src/Pages/Home/Service/Service.jsx
+++ src/Pages/Home/Service/Service.jsx
@@ -1,8 +1,72 @@
 import React from "react";
+import { FaPhoneAlt, FaPlane, FaWifi, FaDumbbell, FaBed } from "react-icons/fa";
 
 const Service = () => {
   return (
-    <h1>this is service section</h1>
+    <section className="bg-secondary text-white py-16 px-4 md:px-20">
+      <div className="grid md:grid-cols-2 gap-10 items-start">
+        {/* Left Side */}
+        <div>
+          <p className="text-primary tracking-widest mb-2">➜ SERVICE</p>
+          <h2 className="text-4xl text-accent md:text-5xl font-bold leading-tight mb-6">
+            Enhancing Your’s <br />
+            Stay With Exclusive <br />
+            Services
+          </h2>
+          <div className="flex items-center gap-4 mt-6">
+            <div className="bg-primary text-black rounded-full p-3">
+              <FaPhoneAlt />
+            </div>
+            <div>
+              <p className="text-accent text-sm">For More Information</p>
+              <p className="text-xl text-accent font-semibold">
+                +880 1407641417
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Side */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          {/* Transportations */}
+          <div className="bg-accent p-6 rounded-md shadow transition duration-300 transform hover:scale-105 group hover:shadow-xl ">
+            <div className="text-primary text-3xl mb-4">{<FaPlane />}</div>
+            <h3 className="text-xl font-semibold mb-2">Transportations</h3>
+            <p className="text-gray-400 text-sm">
+              Reliable airport and local transport services at your convenience
+            </p>
+          </div>
+
+          {/* Wi-Fi and Internet */}
+          <div className="bg-accent p-6 rounded-md shadow transition duration-300 transform hover:scale-105 group hover:shadow-xl">
+            <div className="text-primary text-3xl mb-4">{<FaWifi />}</div>
+            <h3 className="text-xl font-semibold mb-2">Wi-Fi and Internet</h3>
+            <p className="text-gray-400 text-sm">
+              Fast and secure internet access throughout your stay.
+            </p>
+          </div>
+
+          {/* Gym Facilities */}
+
+          <div className="bg-accent p-6 rounded-md shadow transition duration-300 transform hover:scale-105 group hover:shadow-xl">
+            <div className="text-primary text-3xl mb-4">{<FaDumbbell />}</div>
+            <h3 className="text-xl font-semibold mb-2">Gym Facilities</h3>
+            <p className="text-gray-400 text-sm">
+              Modern gym equipment for your fitness routine.
+            </p>
+          </div>
+
+          {/* Room Services */}
+          <div className="bg-accent p-6 rounded-md shadow transition duration-300 transform hover:scale-105 group hover:shadow-xl">
+            <div className="text-primary text-3xl mb-4">{<FaBed />}</div>
+            <h3 className="text-xl font-semibold mb-2">Room Services</h3>
+            <p className="text-gray-400 text-sm">
+              Comfort delivered to your room.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
