3b8ed7d72e1fdae66f7a9fda52bedec0dcca5487
tonmoy <tonmoyth13@gmail.com>
2025-07-02 17:20:18 +0600

    update some problem

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,38 +11,36 @@ import { MdOutlineOtherHouses, MdReviews } from "react-icons/md";
 import { LuLogIn } from "react-icons/lu";
 import { TfiGallery } from "react-icons/tfi";
 
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import {
   ArchiveBoxXMarkIcon,
   ChevronDownIcon,
   PencilIcon,
   Square2StackIcon,
   TrashIcon,
-} from '@heroicons/react/16/solid'
+} from "@heroicons/react/16/solid";
 
 const Header = () => {
   const { pathname } = useLocation();
   const { user, userSignOut } = useContext(AuthContext);
-  const [show, setShow] = useState(false);
-  const containerRef = useRef(null);
-  const [isScrolled,setIsScrolled] = useState(false);
+  // const [show, setShow] = useState(false);
+  // const containerRef = useRef(null);
+  const [isScrolled, setIsScrolled] = useState(false);
 
+  // useEffect(() => {
+  //   const handleClickOutside = (event) => {
+  //     if (containerRef.current && !containerRef.current.contains(event.target)) {
+  //       setShow(false);
+  //     }
+  //   };
+
+  //   document.addEventListener('mousedown', handleClickOutside);
+  //   return () => {
+  //     document.removeEventListener('mousedown', handleClickOutside);
+  //   };
+  // }, []);
 
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (containerRef.current && !containerRef.current.contains(event.target)) {
-        setShow(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-
-   useEffect(() => {
     const onScroll = () => {
       setIsScrolled(window.scrollY > 10);
     };
@@ -86,31 +84,44 @@ const Header = () => {
     <>
       <li className="hover:text-primary">
         <NavLink
-          className={({ isActive }) => isActive && "border-b-2 border-secondary"}
+          className={({ isActive }) =>
+            isActive && "border-b-2 border-secondary"
+          }
           to="/"
         >
           Home
         </NavLink>
       </li>
+      {user && (
+        <>
+          <li className="hover:text-primary">
+            <NavLink
+              className={({ isActive }) =>
+                isActive && "border-b-2 border-secondary"
+              }
+              to="/my_bookings"
+            >
+              My Bookings
+            </NavLink>
+          </li>
+        </>
+      )}
       <li className="hover:text-primary">
         <NavLink
-          className={({ isActive }) => isActive && "border-b-2  border-secondary"}
+          className={({ isActive }) =>
+            isActive && "border-b-2  border-secondary"
+          }
           to="/rooms"
         >
           Rooms
         </NavLink>
       </li>
+
       <li className="hover:text-primary">
         <NavLink
-          className={({ isActive }) => isActive && "border-b-2 border-secondary"}
-          to="/my_bookings"
-        >
-          My Bookings
-        </NavLink>
-      </li>
-      <li className="hover:text-primary">
-        <NavLink
-          className={({ isActive }) => isActive && "border-b-2 border-secondary"}
+          className={({ isActive }) =>
+            isActive && "border-b-2 border-secondary"
+          }
           to="/gallery"
         >
           Gallery
@@ -118,7 +129,9 @@ const Header = () => {
       </li>
       <li className="hover:text-primary">
         <NavLink
-          className={({ isActive }) => isActive && "border-b-2 border-secondary"}
+          className={({ isActive }) =>
+            isActive && "border-b-2 border-secondary"
+          }
           to="/blog"
         >
           Blog
@@ -131,10 +144,10 @@ const Header = () => {
       <div
         className={`navbar transition-all duration-300 px-4 lg:px-12 z-50 text-primary fixed top-0  ${
           pathname === "/" ? "" : "bg-accent-content text-primary"
-        }${isScrolled && 'bg-accent-content text-primary'}`}
+        }${isScrolled && "bg-accent-content text-primary"}`}
       >
         <div className="navbar-start">
-          <Link to='/' className="text-2xl">
+          <Link to="/" className="text-2xl">
             <div className="flex gap-1 items-center">
               <img src={logo} alt="" />
               <span className="text-primary">Book</span>
@@ -142,180 +155,176 @@ const Header = () => {
             </div>
           </Link>
           <div className="absolute top-4 right-6">
-            
-
             {/* side ber */}
-          
-               {/* <div className="fixed top-24 w-52 text-right"> */}
-      <Menu>
-        <MenuButton className="inline-flex items-center lg:hidden gap-2 rounded-md  px-3 py-1.5 text-sm/6 font-semibold text-primary focus:not-data-focus:outline-none data-focus:outline  ">
-          <IoReorderThreeOutline size={30} />
-          
-        </MenuButton>
 
-        <MenuItems
-          transition
-          anchor="bottom end"
-          className="w-52 origin-top-right z-100 rounded-xl   bg-secondary p-1 text-sm text-primary transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
-        >
-          <MenuItem>
-             {user && (
-                <div className="flex items-center p-2 space-x-4">
-                  <img
-                    referrerPolicy="no-referrer"
-                    src={user?.photoURL}
-                    alt=""
-                    className="w-12 h-12 rounded-full dark:bg-gray-500"
-                  />
-                  <div>
-                    <h2 className="text-md font-semibold">
-                      {user?.displayName}
-                    </h2>
-                    <span className="flex items-center space-x-1"></span>
-                  </div>
-                </div>
-              )}
-          </MenuItem>
-          <MenuItem>
-             <ul className="pt-2 pb-4 space-y-1 text-sm">
-                  <li>
-                    <NavLink
-                      to="/"
-                      rel="noopener noreferrer"
-                      href="#"
-                      className={({ isActive }) =>
-                        `flex items-center p-2 space-x-3 rounded-md ${
-                          isActive && "bg-primary text-accent"
-                        }`
-                      }
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                        className="w-5 h-5 fill-current text-accent"
-                      >
-                        <path d="M68.983,382.642l171.35,98.928a32.082,32.082,0,0,0,32,0l171.352-98.929a32.093,32.093,0,0,0,16-27.713V157.071a32.092,32.092,0,0,0-16-27.713L272.334,30.429a32.086,32.086,0,0,0-32,0L68.983,129.358a32.09,32.09,0,0,0-16,27.713V354.929A32.09,32.09,0,0,0,68.983,382.642ZM272.333,67.38l155.351,89.691V334.449L272.333,246.642ZM256.282,274.327l157.155,88.828-157.1,90.7L99.179,363.125ZM84.983,157.071,240.333,67.38v179.2L84.983,334.39Z"></path>
-                      </svg>
-                      <span>Home</span>
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/rooms"
-                      rel="noopener noreferrer"
-                      href="#"
-                      className={({ isActive }) =>
-                        `flex items-center p-2 space-x-3 rounded-md ${
-                          isActive && "bg-primary text-accent"
-                        }`
-                      }
-                    >
-                      <span>
-                        <MdOutlineOtherHouses size={25} />
-                      </span>
-                      <span>Rooms</span>
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/my_bookings"
-                      rel="noopener noreferrer"
-                      href="#"
-                      className={({ isActive }) =>
-                        `flex items-center p-2 space-x-3 rounded-md ${
-                          isActive && "bg-primary text-accent"
-                        }`
-                      }
-                    >
-                      <span>
-                        <PiHouseSimpleBold size={25} />
-                      </span>
-                      <span>My Booking</span>
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/gallery"
-                      rel="noopener noreferrer"
-                      href="#"
-                      className={({ isActive }) =>
-                        `flex items-center p-2 space-x-3 rounded-md ${
-                          isActive && "bg-primary text-accent"
-                        }`
-                      }
-                    >
-                      <span>
-                        <TfiGallery size={21}/>
-                      </span>
-                      <span>Gallery</span>
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/blog"
-                      rel="noopener noreferrer"
-                      href="#"
-                      className={({ isActive }) =>
-                        `flex items-center p-2 space-x-3 rounded-md ${
-                          isActive && "bg-primary text-accent"
-                        }`
-                      }
-                    >
-                      <span>
-                        <MdReviews size={21}/>
-                      </span>
-                      <span>Blog</span>
-                    </NavLink>
-                  </li>
-                </ul>
-          </MenuItem>
-          <div className="my-1 h-px bg-white/5" />
-          <MenuItem>
-            <ul className="pt-4 pb-2 space-y-1 text-sm">
-                  {user ? (
-                    <li onClick={handleSignOutButton}>
-                      <a
+            {/* <div className="fixed top-24 w-52 text-right"> */}
+            <Menu>
+              <MenuButton className="inline-flex lg:hidden items-center gap-2 rounded-md  px-3 py-1.5 text-sm/6 font-semibold text-primary focus:not-data-focus:outline-none data-focus:outline  ">
+                <IoReorderThreeOutline size={30} />
+              </MenuButton>
+
+              <MenuItems
+                transition
+                anchor="bottom end"
+                className="w-52 origin-top-right z-100 rounded-xl bg-secondary p-1 text-sm text-primary transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
+              >
+                <MenuItem as="div">
+                  {user && (
+                    <div className="flex items-center p-2 space-x-4">
+                      <img
+                        referrerPolicy="no-referrer"
+                        src={user?.photoURL}
+                        alt=""
+                        className="w-12 h-12 rounded-full dark:bg-gray-500"
+                      />
+                      <div>
+                        <h2 className="text-md font-semibold">
+                          {user?.displayName}
+                        </h2>
+                        <span className="flex items-center space-x-1"></span>
+                      </div>
+                    </div>
+                  )}
+                </MenuItem>
+                <MenuItem as="div">
+                  <ul className="pt-2 pb-4 space-y-1 text-sm">
+                    <li>
+                      <NavLink
+                        to="/"
                         rel="noopener noreferrer"
                         href="#"
-                        className="flex items-center p-2 space-x-3 rounded-md"
+                        className={({ isActive }) =>
+                          `flex items-center p-2 space-x-3 rounded-md ${
+                            isActive && "bg-primary text-accent"
+                          }`
+                        }
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 512 512"
-                          className="w-5 h-5 fill-current "
+                          className="w-5 h-5 fill-current text-accent"
                         >
-                          <path d="M440,424V88H352V13.005L88,58.522V424H16v32h86.9L352,490.358V120h56V456h88V424ZM320,453.642,120,426.056V85.478L320,51Z"></path>
-                          <rect width="32" height="64" x="256" y="232"></rect>
+                          <path d="M68.983,382.642l171.35,98.928a32.082,32.082,0,0,0,32,0l171.352-98.929a32.093,32.093,0,0,0,16-27.713V157.071a32.092,32.092,0,0,0-16-27.713L272.334,30.429a32.086,32.086,0,0,0-32,0L68.983,129.358a32.09,32.09,0,0,0-16,27.713V354.929A32.09,32.09,0,0,0,68.983,382.642ZM272.333,67.38l155.351,89.691V334.449L272.333,246.642ZM256.282,274.327l157.155,88.828-157.1,90.7L99.179,363.125ZM84.983,157.071,240.333,67.38v179.2L84.983,334.39Z"></path>
                         </svg>
-                        <span>Sign Out</span>
-                      </a>
+                        <span>Home</span>
+                      </NavLink>
                     </li>
-                  ) : (
+                    {user && (
+                      <>
+                        <li>
+                          <NavLink
+                            to="/my_bookings"
+                            rel="noopener noreferrer"
+                            href="#"
+                            className={({ isActive }) =>
+                              `flex items-center p-2 space-x-3 rounded-md ${
+                                isActive && "bg-primary text-accent"
+                              }`
+                            }
+                          >
+                            <span>
+                              <PiHouseSimpleBold size={25} />
+                            </span>
+                            <span>My Booking</span>
+                          </NavLink>
+                        </li>
+                      </>
+                    )}
                     <li>
-                      <Link
-                        to="/signin"
+                      <NavLink
+                        to="/rooms"
                         rel="noopener noreferrer"
                         href="#"
-                        className="flex items-center p-2 space-x-3 rounded-md"
+                        className={({ isActive }) =>
+                          `flex items-center p-2 space-x-3 rounded-md ${
+                            isActive && "bg-primary text-accent"
+                          }`
+                        }
                       >
                         <span>
-                          <LuLogIn size={22} />
+                          <MdOutlineOtherHouses size={25} />
                         </span>
-                        <span>Sign In</span>
-                      </Link>
+                        <span>Rooms</span>
+                      </NavLink>
                     </li>
-                  )}
-                </ul>
-          </MenuItem>
-         
-        </MenuItems>
-      </Menu>
-    {/* </div> */}
 
-
-
-
-
+                    <li>
+                      <NavLink
+                        to="/gallery"
+                        rel="noopener noreferrer"
+                        href="#"
+                        className={({ isActive }) =>
+                          `flex items-center p-2 space-x-3 rounded-md ${
+                            isActive && "bg-primary text-accent"
+                          }`
+                        }
+                      >
+                        <span>
+                          <TfiGallery size={21} />
+                        </span>
+                        <span>Gallery</span>
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/blog"
+                        rel="noopener noreferrer"
+                        href="#"
+                        className={({ isActive }) =>
+                          `flex items-center p-2 space-x-3 rounded-md ${
+                            isActive && "bg-primary text-accent"
+                          }`
+                        }
+                      >
+                        <span>
+                          <MdReviews size={21} />
+                        </span>
+                        <span>Blog</span>
+                      </NavLink>
+                    </li>
+                  </ul>
+                </MenuItem>
+                <div className="my-1 h-px bg-white/5" />
+                <MenuItem as="div">
+                  <ul className="pt-4 pb-2 space-y-1 text-sm">
+                    {user ? (
+                      <li onClick={handleSignOutButton}>
+                        <a
+                          rel="noopener noreferrer"
+                          href="#"
+                          className="flex items-center p-2 space-x-3 rounded-md"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                            className="w-5 h-5 fill-current "
+                          >
+                            <path d="M440,424V88H352V13.005L88,58.522V424H16v32h86.9L352,490.358V120h56V456h88V424ZM320,453.642,120,426.056V85.478L320,51Z"></path>
+                            <rect width="32" height="64" x="256" y="232"></rect>
+                          </svg>
+                          <span>Sign Out</span>
+                        </a>
+                      </li>
+                    ) : (
+                      <li>
+                        <Link
+                          to="/signin"
+                          rel="noopener noreferrer"
+                          href="#"
+                          className="flex items-center p-2 space-x-3 rounded-md"
+                        >
+                          <span>
+                            <LuLogIn size={22} />
+                          </span>
+                          <span>Sign In</span>
+                        </Link>
+                      </li>
+                    )}
+                  </ul>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
+            {/* </div> */}
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -356,5 +365,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
